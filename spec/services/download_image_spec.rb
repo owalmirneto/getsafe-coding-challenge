1cbc0d91dca8e4f84f43408ff064c896ff7cb852
owalmirneto <owalmirneto@gmail.com>
2021-10-27 09:32:46 -0300

    ci: fix rubocop offenses

--- spec/services/download_image_spec.rb
+++ spec/services/download_image_spec.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 describe DownloadImage do
-  include_context 'when download image'
-
   subject(:downloader) { described_class.new(image) }
+
+  include_context 'when download image'
 
   let(:image) { build(:rails_image) }
   let(:image_url) { image.url }
 
   it { is_expected.to respond_to(:perform) }
 
-  context 'description' do
+  context 'when perform downloader' do
     before { downloader.perform }
 
     let(:image_file_name) { image.url.split('/').last }
     let(:image_full_path) { "#{APP_ROOT}/downloads/#{image_file_name}" }
 
-    it { expect(File.exists?(image_full_path)).to eq(true) }
-
     after { FileUtils.rm(image_full_path) }
+
+    it { expect(File.exist?(image_full_path)).to eq(true) }
   end
 end
